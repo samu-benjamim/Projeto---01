cb3d3aea519d4417aa42585b47aa0c87db7af832
Samuel Benjamim Mendes dos Santos <dev.samuel.mendes@gmail.com>
2025-09-30 20:43:08 -0300

    Variaveis CSS

--- pomus-focus/src/App.tsx
+++ pomus-focus/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Heading } from "./components/Heading";
+import { TimerIcon } from "lucide-react";
 
-function App() {
-  const [count, setCount] = useState(0);
+import "./styles/thema.css";
+import "./styles/global.css";
 
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Olá Mundo</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      <Heading>
+        Olá Mundo
+        <button>
+          <TimerIcon />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      </Heading>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
+        laborum molestias expedita nisi eveniet minus voluptate. Corrupti ad
+        perspiciatis fuga quo dolores non alias mollitia consequuntur quaerat
+        veritatis, quibusdam sunt.
       </p>
-    </>
+    </div>
   );
 }
-
-export default App;

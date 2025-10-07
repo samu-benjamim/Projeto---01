095faf9c8cdbb35c519bd0b390f9f56c14e1e7a6
Samuel Benjamim Mendes dos Santos <dev.samuel.mendes@gmail.com>
2025-10-07 18:05:16 -0300

    Form configuration

--- pomus-focus/src/components/Cycles/index.tsx
+++ pomus-focus/src/components/Cycles/index.tsx
@@ -1,18 +1,33 @@
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
 import styles from "./styles.module.css";
 
 export function Cycles() {
+  const { state } = useTaskContext();
+
+  const cycleDescriptionMap = {
+    workTime: "foco",
+    restTime: "descanso curto",
+    longrestTime: "descanso longo",
+  };
+
+  const cycleStep = Array.from({ length: state.currentCycle });
   return (
     <div className={styles.cycles}>
       <span>Ciclos:</span>
       <div className={styles.cyclesDots}>
-        <span className={`${styles.cyclesDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cyclesDot} ${styles.restTime}`}></span>
-        <span className={`${styles.cyclesDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cyclesDot} ${styles.restTime}`}></span>
-        <span className={`${styles.cyclesDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cyclesDot} ${styles.restTime}`}></span>
-        <span className={`${styles.cyclesDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cyclesDot} ${styles.longrestTime}`}></span>
+        {cycleStep.map((_, index) => {
+          const nextCycle = getNextCycle(index);
+          const nextCycleType = getNextCycleType(nextCycle);
+          return (
+            <span
+              key={`${nextCycleType}_${nextCycle}`}
+              className={`${styles.cyclesDot} ${styles[nextCycleType]}`}
+              aria-label={`Indicador de ciclo de ${cycleDescriptionMap[nextCycleType]}`}
+              title={`Indicador de ciclo de ${cycleDescriptionMap[nextCycleType]}`}></span>
+          );
+        })}
       </div>
     </div>
   );
--- pomus-focus/src/components/Mainform/index.tsx
+++ pomus-focus/src/components/Mainform/index.tsx
@@ -1,17 +1,58 @@
-import { PlayCircleIcon } from "lucide-react";
+import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
 import { ButtonPlay } from "../ButtonPlay";
 import { Cycles } from "../Cycles";
 import { Input } from "../Input";
 import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
+import { TaskContext } from "../../contexts/TaskContext/TaskContext";
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
   //  const [taskName, setTaskName] = useState("");
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
 
   const taskNameInput = useRef<HTMLInputElement>(null);
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    console.log("Deu bom", taskNameInput.current?.value);
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current?.value.trim();
+
+    if (!taskName) {
+      alert("Digite o nome da atividade");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      nome: taskName,
+      duration: state.config[nextCycleType],
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   return (
@@ -20,24 +61,34 @@ export function MainForm() {
         <div className="formRow">
           <Input
             labeltext="Atividade:"
-            //            value={taskName}
-            //            onChange={(e) => {
-            //              setTaskName(e.target.value);
-            //            }}
             ref={taskNameInput}
             id="inputtask"
             type="text"
-            placeholder="Digite a atividade aqui"></Input>
+            placeholder="Digite a atividade aqui"
+            disabled={!!state.activeTask}></Input>
           <div className="formRow">
             <p>O próximo interevalor é de 25 min</p>
           </div>
-          <Cycles />
-          <ButtonPlay
-            icon={<PlayCircleIcon />}
-            id="playtime"
-            type="submit"
-            color="green"
-          />
+          {state.currentCycle > 0 && <Cycles />}
+          {!state.activeTask ? (
+            <ButtonPlay
+              icon={<PlayCircleIcon />}
+              aria-label="Iniciar nova atividade"
+              title="Iniciar nova atividade"
+              id="playtime"
+              type="submit"
+              color="green"
+            />
+          ) : (
+            <ButtonPlay
+              icon={<StopCircleIcon />}
+              aria-label="Parar atividade atual"
+              title="Parar atividade atual"
+              id="playtime"
+              type="button"
+              color="red"
+            />
+          )}
         </div>
       </form>
     </>
